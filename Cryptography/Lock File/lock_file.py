e429c08b21ba97b6d2ee3e9faa29b3d58b0c465b
ShadmanSahil <65828927+ShadmanSahil@users.noreply.github.com>
2020-09-10 05:04:13 +0600

    created decryption class
    this class will be used to decrypt the password given by the user

---
+++ Cryptography/Lock File/lock_file.py
@@ -0,0 +1,20 @@
+class GUI_d:
+    def __init__(self, username, password):
+        self.name = username
+        self.Pass = password
+
+    def caesar(self):
+        pass
+
+    def vigenere(self):
+        pass
+        
+    def rsaAlgorithmD(self):
+        pass
+        
+    def new(self):
+        pass
+
+   
+
+   
