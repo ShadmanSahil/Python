ec5b781319225081253145ff4b16b6b6cf749117
ShadmanSahil <65828927+ShadmanSahil@users.noreply.github.com>
2020-09-10 05:06:21 +0600

    added vigenere decryption

--- Cryptography/Lock File/lock_file.py
+++ Cryptography/Lock File/lock_file.py
@@ -3,7 +3,7 @@ class GUI_d:
         self.name = username
         self.Pass = password
 
-    def caesar(self):
+    def caesar(self, word, dir, n):
         x = list(word)
         a = 'abcdefghijklmnopqrstuvwxyz'
         b = a.upper()
@@ -61,8 +61,53 @@ class GUI_d:
                 return ''.join(x)
             
 
-    def vigenere(self):
-        pass
+    def vigenere(self, word1, key1):
+         alpha = 'abcdefghijklmnopqrstuvwxyz'
+        alpha = list(alpha)
+        gamma = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        gamma = list(gamma)
+        word = list(word1)
+        key = list(key1)
+        # keeping record of index position of alphabets in key
+        x = []
+        for z in range(len(key)):
+            if key[z] in alpha:
+                x.append(alpha.index(key[z]))
+            elif key[z] in gamma:
+                x.append(gamma.index(key[z]))
+        # keeping record of index position of spaces
+        a = []
+        for c in range(len(word)):
+            if word[c] == ' ':
+                a.append(c)
+        # removing all spaces
+        while word.count(' ') != 0:
+            word.remove(' ')
+        # encrypting using vigenere cipher
+        for i in range(len(x)):
+            for k in range(i, len(word), len(x)):
+                if word[k] in alpha:
+                    n = alpha.index(word[k])
+                    if n - x[i] < 0:
+                        beta = alpha[n + 1:] + alpha[:n + 1]
+                        word[k] = beta[beta.index(word[k]) - x[i]]
+                    else:
+                        word[k] = alpha[n - x[i]]
+                elif word[k] in gamma:
+                    n = gamma.index(word[k])
+                    if n - x[i] < 0:
+                        beta = gamma[n + 1:] + gamma[:n + 1]
+                        word[k] = beta[beta.index(word[k]) - x[i]]
+                    else:
+                        word[k] = gamma[n - x[i]]
+        # adding spaces in original position
+        new = []
+        for j in range(len(word)):
+            if j in a:
+                new.insert(j, ' ')
+            new.append(word[j])
+        return ''.join(new)
+
         
     def rsaAlgorithmD(self):
         pass
