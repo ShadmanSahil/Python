fe5bde9dd483957b4b6a9cf05f784866c732df13
ShadmanSahil <65828927+ShadmanSahil@users.noreply.github.com>
2020-09-10 05:05:26 +0600

    added caesar decryption

--- Cryptography/Lock File/lock_file.py
+++ Cryptography/Lock File/lock_file.py
@@ -4,7 +4,62 @@ class GUI_d:
         self.Pass = password
 
     def caesar(self):
-        pass
+        x = list(word)
+        a = 'abcdefghijklmnopqrstuvwxyz'
+        b = a.upper()
+        a = list(a)
+        b = list(b)
+        if dir == 'l':
+            if n == 'brute':
+                for i in range(1, 27):
+                    am = []
+                    c = a[-i:] + a[:-i]
+                    d = b[-i:] + b[:-i]
+                    for k in range(len(x)):
+                        if x[k] in a:
+                            am.append(c[a.index(x[k])])
+                        elif x[k] in b:
+                            am.append(d[b.index(x[k])])
+                        else:
+                            am.append(x[k])
+                    z = str(i)
+                    print('Left Shift of ' + z, ':', ''.join(am))
+            else:
+                n = int(n)
+                c = a[-n:] + a[:-n]
+                d = b[-n:] + b[:-n]
+                for k in range(len(x)):
+                    if x[k] in a:
+                        x[k] = c[a.index(x[k])]
+                    elif x[k] in b:
+                        x[k] = d[b.index(x[k])]
+                return ''.join(x)
+        elif dir == 'r':
+            if n == 'brute':
+                for i in range(1, 27):
+                    am = []
+                    c = a[i:] + a[:i]
+                    d = b[i:] + b[:i]
+                    for k in range(len(x)):
+                        if x[k] in a:
+                            am.append(c[a.index(x[k])])
+                        elif x[k] in b:
+                            am.append(d[b.index(x[k])])
+                        else:
+                            am.append(x[k])
+                    z = str(i)
+                    print('Righ Shift of ' + z, ':', ''.join(am))
+            else:
+                n = int(n)
+                c = a[n:] + a[:n]
+                d = b[n:] + b[:n]
+                for k in range(len(x)):
+                    if x[k] in a:
+                        x[k] = c[a.index(x[k])]
+                    elif x[k] in b:
+                        x[k] = d[b.index(x[k])]
+                return ''.join(x)
+            
 
     def vigenere(self):
         pass
