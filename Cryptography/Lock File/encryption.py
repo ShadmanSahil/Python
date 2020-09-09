203fdb70bbf54d72a3178646204ec6989f420ced
ShadmanSahil <65828927+ShadmanSahil@users.noreply.github.com>
2020-09-10 05:12:27 +0600

    added caesar cipher encryption

--- Cryptography/Lock File/encryption.py
+++ Cryptography/Lock File/encryption.py
@@ -5,3 +5,72 @@ class GUI_e:
         self.Pass = password
         self.types = ['caesar', 'vig', 'rsa']
         self.n = len(self.Pass)
+        
+     def caesar(self, word, dir, n):
+        x = list(word)
+        a = 'abcdefghijklmnopqrstuvwxyz'
+        b = a.upper()
+        a = list(a)
+        b = list(b)
+        if dir == 'l':
+            if n == 'brute':
+                # speaker.say('performing brute force encryption with left shift')
+                # speaker.runAndWait()
+                for i in range(1, 27):
+                    am = []
+                    c = a[-i:] + a[:-i]
+                    d = b[-i:] + b[:-i]
+                    for k in range(len(x)):
+                        if x[k] in a:
+                            am.append(c[a.index(x[k])])
+                        elif x[k] in b:
+                            am.append(d[b.index(x[k])])
+                        else:
+                            am.append(x[k])
+                    z = str(i)
+                    print('Left Shift of ' + z, ':', ''.join(am))
+            else:
+                # speaker.say('performing caesar cipher encryption with left shift of {}'.format(int(n)))
+                # speaker.runAndWait()
+                n = int(n)
+                c = a[-n:] + a[:-n]
+                d = b[-n:] + b[:-n]
+                for k in range(len(x)):
+                    if x[k] in a:
+                        x[k] = c[a.index(x[k])]
+                    elif x[k] in b:
+                        x[k] = d[b.index(x[k])]
+                return ''.join(x)
+                # speaker.say('encryption complete')
+                # speaker.runAndWait()
+        elif dir == 'r':
+            if n == 'brute':
+                # speaker.say('performing brute force encryption with right shift')
+                # speaker.runAndWait()
+                for i in range(1, 27):
+                    am = []
+                    c = a[i:] + a[:i]
+                    d = b[i:] + b[:i]
+                    for k in range(len(x)):
+                        if x[k] in a:
+                            am.append(c[a.index(x[k])])
+                        elif x[k] in b:
+                            am.append(d[b.index(x[k])])
+                        else:
+                            am.append(x[k])
+                    z = str(i)
+                    print('Righ Shift of ' + z, ':', ''.join(am))
+            else:
+                # speaker.say('performing caesar cipher encryption with right shift of {}'.format(int(n)))
+                # speaker.runAndWait()
+                n = int(n)
+                c = a[n:] + a[:n]
+                d = b[n:] + b[:n]
+                for k in range(len(x)):
+                    if x[k] in a:
+                        x[k] = c[a.index(x[k])]
+                    elif x[k] in b:
+                        x[k] = d[b.index(x[k])]
+                return ''.join(x)
+                # speaker.say('encryption complete')
+                # speaker.runAndWait()
